62ae949b7d2de3522148867aa7618eba64db95ad
Betrouni <betrouninassim@outlook.fr>
2024-10-07 23:59:46 +0200

    Switched to HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Pokedex from './components/Pokedex';
 import PokemonDetail from './components/PokemonDetail';
 import { ThemeProvider, useTheme } from './ThemeContext';
